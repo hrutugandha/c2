63259e5df63c6693acfb552d736698f541bae860
BishamontenSama <62648846+hrutugandha@users.noreply.github.com>
2022-04-18 16:16:46 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import { AddStudent } from "./components/AddStudent";
+import { ShowStudents } from "./components/ShowStudents";
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Table</h1>
+      <button className="togglebtn" onClick="addStudents()">Add Student</button>
+
+      <div> {addStudents() === true ? AddStudent : ShowStudents}</div>
+      <form className="addstudent">
+        <div>
+          Firstname:{" "}
+          <input
+            type="text"
+            name="first_name"
+            className="first_name"
+            placeholder="enter first name"
+          />
+        </div>
+        <div>
+          {" "}
+          Last Name:
+          <input
+            type="text"
+            name="last_name"
+            className="last_name"
+            placeholder="enter last name"
+          />
+        </div>
+        <div>
+          {" "}
+          Email:
+          <input
+            type="email"
+            name="email"
+            className="email"
+            placeholder="enter email"
+          />
+        </div>
+  
+        <div>
+          Gender: {/* NOTE: radio boxes only work when they have same `name`. */}
+          <div>
+            Male
+            <input
+              name="gender"
+              className="male"
+              type="radio"
+              value={"male"}
+            />{" "}
+            Female{" "}
+            <input
+              name="gender"
+              className="female"
+              type="radio"
+              value={"female"}
+            />
+          </div>
+        </div>
+        <div>
+          Age{" "}
+          <input
+            type="number"
+            name="age"
+            className="age"
+            placeholder="enter age"
+          />
+        </div>
+        <div>
+          Tenth Score:{" "}
+          <input
+            type="number"
+            name="tenth_score"
+            className="tenth_score"
+            placeholder="enter 10th score"
+          />{" "}
+        </div>
+        <div>
+          Twelth Score:{" "}
+          <input
+            type="number"
+            name="twelth_score"
+            className="twelth_score"
+            placeholder="enter 12th score"
+          />{" "}
+        </div>
+        <div>
+          <select
+            value={""} // select dropdown needs both value and onChange attributes
+            name="preferred_branch"
+            className="preferred_branch"
+          >
+            <option value="law">law</option>
+            <option value="commerce">commerce</option>
+            <option value="science">science</option>
+            <option value="sports">sports</option>
+            <option value="arts">arts</option>
+            <option value="acting">acting</option>
+          </select>
+        </div>
+  
+        <input className="submit" type="submit" value="Submit" />
+        {
+          // <div className="error"></div>
+          // show this div with proper error before submitting form, if there's anything not provided
+          // eg: first name missing, age cannot be greater than 100 etc
+        }
+      </form>
+      
+      {/* Show either  AddStudent component or ShowStudents dependeing on the above button click  */}
+      {/* make sure the table is shown initially, do not show form initially */}
+      {/* make sure to show either of them do not both together */}
+
     </div>
   );
+
+  function addStudents(e){
+    return true;
+  }
 }
 
 export default App;
